3ca09881322f8051603bbfb0ed147214c127c535
joshuahowlett <howlettj1@students.rowan.edu>
2018-11-20 23:55:07 -0500

    Adds Polished Crimson Coal Functionality

--- src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
+++ src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
@@ -13,5 +13,6 @@ public class ModBlocks
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 
 	public static final Block CRIMSON_COAL_BLOCK = new CrimsonCoalBlock("crimson_coal_block", Material.ROCK);
+	public static final Block POLISHED_CRIMSON_COAL_BLOCK = new CrimsonCoalBlock("polished_crimson_coal_block", Material.ROCK);
 
 }
