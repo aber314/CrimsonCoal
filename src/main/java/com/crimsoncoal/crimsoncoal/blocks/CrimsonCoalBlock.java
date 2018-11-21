919be6e907e0aa439558fd0509bb9bd348d61e2b
Swaggless <swagglesszach@gmail.com>
2018-11-20 21:43:32 -0500

    Added Crimson Coal Block

---
+++ src/main/java/com/crimsoncoal/crimsoncoal/blocks/CrimsonCoalBlock.java
@@ -0,0 +1,22 @@
+package com.crimsoncoal.crimsoncoal.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class CrimsonCoalBlock extends BlockBase
+{
+
+	public CrimsonCoalBlock(String name, Material material)
+	{
+		super(name, material);
+		setSoundType(SoundType.STONE);
+		setHardness(5.0F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(0.0F);
+		// setLightOpacity(0);
+		// setBlockUnbreakable();
+
+	}
+
+}
--- src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
+++ src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
@@ -3,7 +3,7 @@ package com.crimsoncoal.crimsoncoal.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crimsoncoal.crimsoncoal.blocks.BlockBase;
+import com.crimsoncoal.crimsoncoal.blocks.CrimsonCoalBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -12,6 +12,6 @@ public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-	public static final Block CRIMSON_COAL_BLOCK = new BlockBase("crimson_coal_block", Material.ROCK);
+	public static final Block CRIMSON_COAL_BLOCK = new CrimsonCoalBlock("crimson_coal_block", Material.ROCK);
 
 }
