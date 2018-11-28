6b0a528311a89b2ef1a3261a896f0dd8a29ed9f8
Swaggless <swagglesszach@gmail.com>
2018-11-27 20:30:12 -0500

    Added CrimsonCoalOre blocks

---
+++ src/main/java/com/crimsoncoal/crimsoncoal/blocks/CrimsonCoalOre.java
@@ -0,0 +1,35 @@
+package com.crimsoncoal.crimsoncoal.blocks;
+
+import java.util.Random;
+
+import com.crimsoncoal.crimsoncoal.init.ModItems;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+public class CrimsonCoalOre extends BlockBase
+{
+
+	public CrimsonCoalOre(String name, Material material)
+	{
+		super(name, material);
+		setSoundType(SoundType.METAL);
+		setHardness(5.0F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 0);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return ModItems.CRIMSON_COAL;
+	}
+
+	@Override
+	public int quantityDropped(Random rand)
+	{
+		return 1;
+	}
+}
--- src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
+++ src/main/java/com/crimsoncoal/crimsoncoal/init/ModBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.crimsoncoal.crimsoncoal.blocks.CrimsonCoalBlock;
+import com.crimsoncoal.crimsoncoal.blocks.CrimsonCoalOre;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -14,5 +15,6 @@ public class ModBlocks
 
 	public static final Block CRIMSON_COAL_BLOCK = new CrimsonCoalBlock("crimson_coal_block", Material.ROCK);
 	public static final Block POLISHED_CRIMSON_COAL_BLOCK = new CrimsonCoalBlock("polished_crimson_coal_block", Material.ROCK);
+	public static final Block CRIMSON_COAL_ORE = new CrimsonCoalOre("crimson_coal_ore", Material.ROCK);
 
 }
