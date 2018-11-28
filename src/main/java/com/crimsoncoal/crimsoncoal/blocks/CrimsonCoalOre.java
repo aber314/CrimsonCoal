c1eb20d42dcca9aafa8fa38ca65aaed0cc36f837
Gorflow <aber314@gmail.com>
2018-11-27 20:54:36 -0500

    Added xp drop (0-3) to crimson coal ore

--- src/main/java/com/crimsoncoal/crimsoncoal/blocks/CrimsonCoalOre.java
+++ src/main/java/com/crimsoncoal/crimsoncoal/blocks/CrimsonCoalOre.java
@@ -8,6 +8,8 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class CrimsonCoalOre extends BlockBase
 {
@@ -19,13 +21,22 @@ public class CrimsonCoalOre extends BlockBase
 		setHardness(5.0F);
 		setResistance(15.0F);
 		setHarvestLevel("pickaxe", 0);
-
 	}
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune)
 	{
 		return ModItems.CRIMSON_COAL;
+	}
+
+	@Override
+	public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune)
+	{
+		Random rand = new Random();
+		int max = 3;
+		int min = 0;
+
+		return rand.nextInt(max) + min;
 	}
 
 	@Override
