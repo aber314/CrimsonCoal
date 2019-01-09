8f9dffbefb5f0251affb11b47fb9463091131681
Swaggless <swagglesszach@gmail.com>
2019-01-08 19:21:57 -0500

    Polished Crimson Coal Block is now burnable! Same duration a normal Crimson Coal Block

--- src/main/java/com/crimsoncoal/crimsoncoal/blocks/PolishedCrimsonCoalBlock.java
+++ src/main/java/com/crimsoncoal/crimsoncoal/blocks/PolishedCrimsonCoalBlock.java
@@ -2,13 +2,27 @@ package com.crimsoncoal.crimsoncoal.blocks;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 
 public class PolishedCrimsonCoalBlock extends BlockBase
 {
 
 	public PolishedCrimsonCoalBlock(String name, Material material)
 	{
-		super(name, material);
+		super(name, material, false);
+
+		ItemBlock polishedCrimsonCoalBlock = new ItemBlock(this)
+		{
+			@Override
+			public int getItemBurnTime(ItemStack itemStack)
+			{
+				return 24000;
+			}
+		};
+
+		this.register(polishedCrimsonCoalBlock);
+
 		setSoundType(SoundType.STONE);
 		setHardness(5.0F);
 		setResistance(15.0F);
